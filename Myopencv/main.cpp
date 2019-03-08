4ba4a2931b9d0c8312d21d4f46dd6f99028d2754
ZZH <ZZH@DESKTOP-P69IR28>
2019-03-08 14:09:30 +0800

    update

--- Myopencv/main.cpp
+++ Myopencv/main.cpp
@@ -10,6 +10,174 @@ using namespace cv;
 
 string xmlPath = "haarcascade_frontalface_alt.xml";
 //xmlpath 字符串记录那个.xml文件的路径
+Mat src, gray_src, drawImg;
+int threshold_v = 170;
+int threshold_max = 255;
+const char* output_win = "rectangle-demo";
+RNG rng(12345);
+void print_Mat(Mat img) {
+	for (int row = 0; row < img.rows; row++) {
+		for (int col = 0; col < img.cols; col++) {
+			cout << (int)img.at<uchar>(row, col) << " ";
+		}
+		cout << endl;
+	}
+}
+int Sum_mat(Mat img) {
+	int sum = 0;
+	for (int row = 0; row < img.rows; row++) {
+		for (int col = 0; col < img.cols; col++) {
+			sum += (int)img.at<uchar>(row, col);
+		}
+	}
+	return sum;
+}
+int Count_Mat(Mat img) {
+	int count = 0;
+	for (int row = 0; row < img.rows; row++) {
+		for (int col = 0; col < img.cols; col++) {
+			if ((int)img.at<uchar>(row, col) == 0) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+int _caohe_() {
+	src = imread("exmaple.png");
+	if (!src.data) {
+		printf("could not load image...\n");
+		return -1;
+	}
+	cvtColor(src, gray_src, CV_BGR2GRAY);
+	blur(gray_src, gray_src, Size(3, 3), Point(-1, -1));
+
+	Mat binary_output;
+	vector<vector<Point>> contours;
+	vector<Vec4i> hierachy;
+	threshold(gray_src, binary_output, threshold_v, threshold_max, THRESH_BINARY);
+	int count = Count_Mat(binary_output);
+	findContours(binary_output, contours, hierachy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point());
+	int area = contourArea(contours[1]);
+	cout << "The object area is:" << area << endl;
+
+	Mat imageContours = src;
+	RotatedRect rect;
+
+	for (size_t i = 0; i < contours.size(); i++) {
+		rect = minAreaRect(contours[i]);
+		Point2f P[4];
+		rect.points(P);
+		for (int j = 0; j <= 3; j++) {
+			line(imageContours, P[j], P[(j + 1) % 4], Scalar(255), 2);
+		}
+		// cv::imwrite("imageContours.jpg", imageContours);
+	}
+
+	Point2f center = rect.center;//外接矩形中心点坐标
+	Mat rot_mat = getRotationMatrix2D(center, rect.angle, 1.0);//求旋转矩阵
+	Mat rot_image;
+	Size dst_sz(imageContours.size());
+	warpAffine(gray_src, rot_image, rot_mat, dst_sz);//原图像旋转
+	Mat ROI = rot_image(Rect(center.x - (rect.size.width / 2), center.y - (rect.size.height / 2), rect.size.width, rect.size.height));//提取ROI
+	//imwrite("ROI.jpg", ROI);
+
+	Mat rotate;
+	flip(ROI, rotate, 1);
+	transpose(rotate, rotate);
+	//imwrite("rotate.jpg", rotate);
+
+	Mat show = Mat::zeros(rotate.size(), CV_8UC1);
+
+	threshold(rotate, show, 128, 255, THRESH_BINARY);
+	for (int row = 0; row < show.rows; row++) {
+		for (int col = 0; col < show.cols; col++) {
+			int gray = rotate.at<uchar>(row, col);
+			show.at<uchar>(row, col) = 255 - gray;
+			if ((int)show.at<uchar>(row, col) > 128) {
+				show.at<uchar>(row, col) = 1;
+			}
+			else {
+				show.at<uchar>(row, col) = 0;
+			}
+		}
+	}
+
+	int c = rotate.rows; int d = rotate.cols;
+	float HWpercent[] = { 0.5,0.6,0.7,0.8,0.9,1.0 };
+	vector<vector<float>> mmmm;
+	vector<float> max_Iou_index;
+	for (int i = 0; i < sizeof(HWpercent) / sizeof(float); i++) {
+		float percent = HWpercent[i];
+		int square_height = floor(sqrt(area / percent));
+		int square_width = floor(square_height*percent);
+
+		for (int m = 0; m < c - square_width + 1; m++) {
+			for (int n = 0; n < d - square_height + 1; n++) {
+				Mat square = Mat::zeros(Size(d, c), CV_8UC1);
+				Mat out = square.clone();
+				square.rowRange(m, m + square_width) = 1;
+				square.colRange(n, n + square_height) = 1;
+				out = square.mul(show);
+				int count2 = Sum_mat(out);
+				float Iou = (float)count2 / count;
+				vector<float>tmp;
+				tmp.push_back(Iou);
+				tmp.push_back(n);
+				tmp.push_back(m);
+				mmmm.push_back(tmp);
+				float max_Iou = 0.0;
+				if (max_Iou < Iou) {
+					max_Iou = Iou;
+					vector<float>tmp2;
+					tmp2.push_back(square_height);
+					tmp2.push_back(square_width);
+					tmp2.push_back(percent);
+					tmp2.push_back(m);
+					tmp2.push_back(n);
+					tmp2.push_back(Iou);
+					max_Iou_index = tmp2;
+				}
+			}
+		}
+	}
+	Point point1 = Size(max_Iou_index[3], max_Iou_index[4]);
+	Point point2 = Size(max_Iou_index[0], max_Iou_index[1]);
+	rectangle(rotate, point1, point2, Scalar(0, 255, 0), 2);
+	imshow("src", rotate);
+}
+void unevenLightCompensate(Mat &image, int blockSize)
+{//一种不均匀光照的补偿方法
+	if (image.channels() == 3) cvtColor(image, image, 7);
+	double average = mean(image)[0];
+	int rows_new = ceil(double(image.rows) / double(blockSize));
+	int cols_new = ceil(double(image.cols) / double(blockSize));
+	Mat blockImage;
+	blockImage = Mat::zeros(rows_new, cols_new, CV_32FC1);
+	for (int i = 0; i < rows_new; i++)
+	{
+		for (int j = 0; j < cols_new; j++)
+		{
+			int rowmin = i * blockSize;
+			int rowmax = (i + 1)*blockSize;
+			if (rowmax > image.rows) rowmax = image.rows;
+			int colmin = j * blockSize;
+			int colmax = (j + 1)*blockSize;
+			if (colmax > image.cols) colmax = image.cols;
+			Mat imageROI = image(Range(rowmin, rowmax), Range(colmin, colmax));
+			double temaver = mean(imageROI)[0];
+			blockImage.at<float>(i, j) = temaver;
+		}
+	}
+	blockImage = blockImage - average;
+	Mat blockImage2;
+	resize(blockImage, blockImage2, image.size(), (0, 0), (0, 0), INTER_CUBIC);
+	Mat image2;
+	image.convertTo(image2, CV_32FC1);
+	Mat dst = image2 - blockImage2;
+	dst.convertTo(image, CV_8UC1);
+	imwrite("dst.jpg", image);
+}
 void detectAndDisplay(Mat image)
 {
 	CascadeClassifier ccf;      //创建脸部对象
@@ -251,10 +419,27 @@ int _Gamma_Enhance_() {//伽马变换增强图像对比度
 	return 0;
 }
 
+int _Image_process_() {
+	Mat image = imread("007.jpg");
+//	Mat result_img(image.resize(),CV_32FC3);
+	if (image.empty) {
+		cout << "打开图片失败，请重新检查!" << endl;
+		return -1;
+	}
+	
+	IplImage * img =cvLoadImage("007.jpg");
+	IplImage *pimg = cvCreateImage(cvGetSize(img), 8, 1);
+	cvSobel(img, pimg,3,3,3);
+	cvSaveImage("soble.jpg",pimg);
+	
+
+
+}
+
 
 int main()
 {
-	_Gamma_Enhance_();
+	_Image_process_();
 	
 	//cin.get();
 
